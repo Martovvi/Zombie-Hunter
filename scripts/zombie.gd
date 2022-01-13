c0faed61ebd064880c7e4f1aadcff058b91a18b3
Martovvi <55866297+Martovvi@users.noreply.github.com>
2022-01-13 19:41:01 +0100

    Added simple zombie animations

--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 # Simple zombie AI that follows the player and kills him when the player is too close.
 
-const MOVE_SPEED = 155
+const MOVE_SPEED = 130
 
-var velocity = Vector2.ZERO
+var is_chasing = false
 
 onready var raycast = $RayCast2D
 onready var zombie_body = $Body
@@ -16,54 +16,26 @@ func chase_player(delta):
 	vec_to_player = vec_to_player.normalized()
 	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
 	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+	is_chasing = true
 		
 	if raycast.is_colliding():
 		var coll = raycast.get_collider()
 		if coll.name == "Player":
 			coll.kill()
 
-# Animations controller for various zombie based animations.
 func animations_controller():
-	if(velocity != Vector2.ZERO):
+	if(is_chasing):
 		zombie_body.play("default")
 		zombie_legs.play("default")
-	if(velocity != Vector2.ZERO):
-		zombie_legs.play("default")
-	if(velocity == Vector2.ZERO):
+	else:
+		zombie_body.stop()
 		zombie_legs.stop()
-		zombie_legs.play("idle")
-	elif(velocity == Vector2.ZERO):
 		zombie_body.play("idle")
 		zombie_legs.play("idle")
 
- # Rotates legs based on movement direction.
-func rotate_legs():
-	if(velocity.x != 0 && velocity.y == 0):
-		if(velocity.x > 0):
-			zombie_legs.set_rotation_degrees(-90)
-		else:
-			zombie_legs.set_rotation_degrees(90)
-	elif(velocity.x == 0 && velocity.y != 0):
-		if(velocity.y > 0):
-			zombie_legs.set_rotation_degrees(0)
-		else:
-			zombie_legs.set_rotation_degrees(180)
-	elif(velocity.x != 0 && velocity.y != 0):
-		if(velocity.x > 0 && velocity.y < 0):
-			zombie_legs.set_rotation_degrees(-135)
-		elif(velocity.x < 0 && velocity.y > 0):
-			zombie_legs.set_rotation_degrees(45)
-		elif(velocity.x > 0 && velocity.y > 0):
-			zombie_legs.set_rotation_degrees(-45)
-		elif(velocity.x < 0 && velocity.y < 0):
-			zombie_legs.set_rotation_degrees(135)
-	else:
-		zombie_legs.set_rotation_degrees(0)
-
-
 func _physics_process(delta):
 	chase_player(delta)
-
+	animations_controller()
 
 func hit():
 	var blood_instance = blood.instance()
